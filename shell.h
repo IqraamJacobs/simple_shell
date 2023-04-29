c52c30940a94cbde336c158cd5c3013d9ec064ea
Ebenezer Emmanuel Emonueje <eebenezer949@gmail.com>
2023-04-29 04:54:30 +0000

    fix

--- shell.h
+++ shell.h
@@ -20,23 +20,4 @@ char *_strcpy(char *dest, char *src);
 char *_getenv(const char *name);
 char *get_full_path(char *cmd);
 
-/* usefull header files for the shell */
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-
-/* environment */
-extern char **environ;
-
-/* functions prototypes used for the shell */
-char **parse_cmd(char *user_cmd, int num_tokens, char *prog_name);
-void free_cmd_arr(char **cmd_arr);
-void execute_cmd(char **cmd, char *prog_name);
-char *_strcpy(char *dest, char *src);
-char *_getenv(const char *name);
-char *get_full_path(char *cmd);
-
 #endif
